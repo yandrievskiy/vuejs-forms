2f72fc9253519aad99779a717aa28351eec09bac
yandrievskiy <yandrievskiy@justcoded.co>
2020-03-04 11:49:05 +0200

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    Initial Setup
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {
+  },
+};
+</script>
+
+<style lang="scss">
+
+</style>
